6f1c3b998e666277c336bcd16f43f7d7f57319c6
Queue.S <qq.kwkw@gmail.com>
2015-01-16 06:29:08 +0900

    Update MemoryPool.h

--- MemoryPool.h
+++ MemoryPool.h
@@ -4,7 +4,7 @@
  * Released under the MIT license
  */
 
-// Constructor using memory pool.
+// Allocate memory for class object from memory pool.
 class ManagedObject {
 public:
 	static void* operator new(size_t size) {
@@ -15,7 +15,10 @@ public:
 	}
 };
 
-// Allocate memory from memory pool.
+// HACK : Allocate memory for class object from memory pool.
+//        But the constructor is not called.
+//        Just copying the vitual method table's pointer address.
+// use-case -> using memory by best effort.
 template <class V > V* MALLOC() {
 	V* v = (V*)MemoryPool::allocate(sizeof(V));
 	V impl;
@@ -29,10 +32,20 @@ class MemoryPool
 {
 public:
 
+	// init memory pool state.
 	static void initialize();
+	
+	// reset memory pool. All memories are droped.
+	// WARN : The object's destructor can not be called. Do it yourself.
 	static void reset();
+	
+	// allcate memory.
 	static void* allocate( int size );
+	
+	// drop memory.
 	static void free( void* chunk );
+	
+	// show used memory chunk count.
 	static unsigned int getCount();
 
 private:

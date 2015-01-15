5b7ee68cc72d850b1bf590d57b67993d5d3962e4
Queue.S <qq.kwkw@gmail.com>
2015-01-16 05:31:11 +0900

    Update MathUtil.h

--- MathUtil.h
+++ MathUtil.h
@@ -3,26 +3,23 @@
  * Copyright (c) 2010-2012,2015 Queue Sakura-Shiki
  * Released under the MIT license
  */
-#pragma once
-#include <Windows.h>
-
 class MathUtil
 {
 public:
 	static void initialize();
 
-	static double sin( INT32 angle );
+	static double sin( int angle );
 	static double sinUp( double percent );
 	static double sinDown( double percent );
 
-	static double cos( INT32 angle );
+	static double cos( int angle );
 	static double cosUp( double percent );
 	static double cosDown( double percent );
 
-	static INT32 upDown( INT32 length, INT32 value, INT32 range );
+	static int upDown( int length, int value, int range );
 
 	static void MathUtil::atan ( double *resultX, double *resultY, double sourceX, double targetX, double sourceY, double targetY );
-	static INT32 MathUtil::atanAngle ( double sourceX, double targetX, double sourceY, double targetY );
+	static int MathUtil::atanAngle ( double sourceX, double targetX, double sourceY, double targetY );
 private:
 	MathUtil(void);
 	~MathUtil(void);
